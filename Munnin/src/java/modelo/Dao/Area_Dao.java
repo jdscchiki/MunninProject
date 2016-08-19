a148f9b52dfb12fac47629bf5e937fe88d92c688
Sergio <Sergio@Sergio-PC>
2016-08-19 01:28:48 -0500

    AreaDAO - Metodos de procedimiento almacenado

--- Munnin/src/java/modelo/Dao/Area_Dao.java
+++ Munnin/src/java/modelo/Dao/Area_Dao.java
@@ -50,19 +50,54 @@ public class Area_Dao extends ClassConexion {
         }
         return listo;
     }
-    
-    public boolean editar_area(){
+
+    public boolean editar_area() {
         try {
             CallableStatement cst = conn.prepareCall("{call editar_area (?,?,?)}");
             do {
+                // Se envian parametros del procedimiento almacenado
                 cst.setLong(1, id_area);
                 cst.setString(2, nombre_area);
                 cst.setString(3, id_centro_area);
-
+                // Ejecuta el procedimiento almacenado
                 cst.execute();
             } while (id_area > 0);
         } catch (SQLException e) {
         }
         return listo;
+    }
+
+    public boolean eliminar_area() {
+        try {
+            CallableStatement cst = conn.prepareCall("{call eliminar_area (?)}");
+            do {
+                // Se envian parametros del procedimiento almacenado
+                cst.setLong(1, id_area);
+                // Ejecuta el procedimiento almacenado
+                cst.execute();
+            } while (id_area > 0);
+        } catch (SQLException e) {
+        }
+        return listo;
+    }
+
+    public Area_Bean ver_area() {
+        Area_Bean ab = null;
+        try {
+            CallableStatement cst = conn.prepareCall("{call ver_area (?)}");
+            do {
+                // Se envian parametros del procedimiento almacenado
+                cst.setLong(1, id_area);
+                // Definimos los tipos de los parametros de salida del procedimiento almacenado
+                cst.registerOutParameter(2, java.sql.Types.VARCHAR);
+                cst.registerOutParameter(3, java.sql.Types.VARCHAR);
+                // Ejecuta el procedimiento almacenado
+                cst.execute();
+                // Se obtienen la salida del procedimineto almacenado                
+                ab = new Area_Bean(id_area,cst.getString(2),cst.getString(3));
+            } while (id_area > 0);
+        } catch (SQLException e) {
+        }
+        return ab;
     }
 }
