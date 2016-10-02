58fefe001f89f52fa9dc6007c20b59784f7e5c3e
Juan David <Juan David@DESKTOP-20NH061>
2016-10-01 22:41:25 -0500

    Agregado: 	Clase Funcionario 	Clase Rol

---
+++ Munnin/src/java/modelo/bean/Funcionario.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.bean;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Juan David
+ */
+public class Funcionario {
+    private int id;
+    private int documento;
+    private String correo;
+    private String contrasena;
+    private String nombre;
+    private String apellido;
+    private String cargo;
+    private String telefono;
+    private String id_centro;
+
+    private ArrayList<Rol> roles;
+
+    public ArrayList<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(ArrayList<Rol> roles) {
+        this.roles = roles;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getId_centro() {
+        return id_centro;
+    }
+
+    public void setId_centro(String id_centro) {
+        this.id_centro = id_centro;
+    }
+
+    public Funcionario() {
+    
+    }
+    
+    
+}
---
+++ Munnin/src/java/modelo/bean/Rol.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.bean;
+
+/**
+ *
+ * @author Juan David
+ */
+public class Rol {
+    private int id;
+    private String nombre;
+
+    public Rol() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
