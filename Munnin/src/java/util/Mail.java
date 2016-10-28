8b1aca1b36949a8fca1c3424958acacaeb0b54d8
Juan David <Juan David@DESKTOP-20NH061>
2016-10-28 00:49:13 -0500

    Agregado Mail

---
+++ Munnin/src/java/util/Mail.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import modelo.bean.Funcionario;
+/**
+ *
+ * @author Juan David Segura Castro <JBadCode>
+ */
+public class Mail {
+    public static boolean enviarContrasena(Funcionario destinatario, String Contrasena){
+        boolean resultado = true;
+        return resultado;
+    }
+}
